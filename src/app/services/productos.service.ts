6afe984029058cb9b49a4ca788ab2132ea456965
Patricio Arrobo <apafespe@gmail.com>
2020-03-30 01:34:01 -0500

    fin de la seccion 6

--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -21,12 +21,16 @@ private cargarProductos(){
 
     this.http.get('https://angular-html-3a360.firebaseio.com/productos_idx.json')
        .subscribe( (resp: Producto[]) => {
-
-         console.log(resp);
-         this.productos = resp;
+        this.productos = resp;
         this.cargando=false;
   
 
        });
+  }
+
+  getProducto( id: string ){
+
+   return  this.http.get(`https://angular-html-3a360.firebaseio.com/productos/${ id }.json`)
+
   }
 }
